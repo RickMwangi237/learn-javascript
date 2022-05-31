2ee99c72f7f2ef734ecd012473cccccab43b6298
Rick Mwangi <rickmwangi237@protonmail.com>
2022-05-31 15:52:10 +0300

    Create if esle if else conditional

--- 3. Build a Blackjack game/4. Your first if...else statement/index.js
+++ 3. Build a Blackjack game/4. Your first if...else statement/index.js
@@ -1,7 +1,7 @@
 // Check if the person is old enough to enter the nightclub (21)
 // Log a suitable message to the console in both cases
 
-let age = 21
+let age = 20
 
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
