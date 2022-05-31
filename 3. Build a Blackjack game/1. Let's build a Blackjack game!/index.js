4b09b68f3c7139a5b6114a1884b8afcf61d7df51
Rick Mwangi <rickmwangi237@protonmail.com>
2022-05-31 15:33:17 +0300

    Add conditional loop for the value of sum variable

--- 3. Build a Blackjack game/1. Let's build a Blackjack game!/index.js
+++ 3. Build a Blackjack game/1. Let's build a Blackjack game!/index.js
@@ -1,7 +1,13 @@
-let firstCard = 7
-
-let secondCard = 9
-
+let firstCard = 11
+let secondCard = 11
 let sum = firstCard + secondCard
 
 console.log(sum)
+
+if (sum < 21){
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Winner Winner Chicken Dinner!!")
+} else {
+    console.log("Rudi home Kijana!")
+}
