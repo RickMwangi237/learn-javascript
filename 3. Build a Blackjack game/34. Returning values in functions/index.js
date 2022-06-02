3aa210b1c26deeea99ecd0e8b36a4e491fa8d525
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-02 13:10:15 +0300

    create function that returns totla race time

--- 3. Build a Blackjack game/34. Returning values in functions/index.js
+++ 3. Build a Blackjack game/34. Returning values in functions/index.js
@@ -18,6 +18,9 @@ console.log(fastestRace)
 // Write a function that returns the total race time
 // Call/invoke the function and store the returned value in a new variable
 // Finally, log the variable out
+function totalRaceTime() { 
+    return player1Time + player2Time
+}
+let total = totalRaceTime()
 
-
-
+console.log(total)
