b90629eb9598549384863b7f2d1b6c7ca636e812
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-03 14:57:22 +0300

    Create object and function logging out string

--- 4. Practice time - part 2/1. Objects and functions/index.js
+++ 4. Practice time - part 2/1. Objects and functions/index.js
@@ -12,3 +12,9 @@ let person = {
     age: 28,
     country: "Kenya"
 }
+
+function logData() { console.log(
+    person.name + " is " + person.age + " years old and lives in " + person.country +".")
+}
+
+logData()
