90692c3b608f8d447b3ea113407824a86be746ca
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-03 15:12:41 +0300

    Create if conditional for ticketing

--- 4. Practice time - part 2/2. if else/index.js
+++ 4. Practice time - part 2/2. if else/index.js
@@ -1,4 +1,4 @@
-let age = 15
+let age = 6
 
 // less than 6 years old -> free
 // 6 to 17 years old     -> child discount
@@ -8,3 +8,13 @@ let age = 15
 
 // Create a conditional statement (if/else/else if) that logs out the discount
 // the passenger will get based upon the value of the age variable
+
+if (age > 66){
+    console.log("Senior citizen discount")
+} else if(age <= 66 && age >=27 ) {
+    console.log("Full Price")
+} else if(age < 27 && age >= 18) {
+    console.log("Student discount")
+} else if( age < 18 && age >= 6) {
+    console.log("Child discount")
+} else {console.log(Free)}
