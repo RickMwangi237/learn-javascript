6ab27e068dc2e6b92975eb09a67c0464ffe49cc5
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-15 15:02:14 +0300

    Create box variable from dom and add listener

--- 5. Build a Chrome Extension/6. Write your first addEventListener()/index.js
+++ 5. Build a Chrome Extension/6. Write your first addEventListener()/index.js
@@ -1,3 +1,9 @@
-// 1. Grab the box from the DOM and store it in a variable
+// 1. Grab the box from the DOM and store it in a 
+
+let box = document.getElementById("box")
 // 2. Add a click event listener to the box 
-// 3. Log out "I want to open the box!" when it's clicked
+
+box.addEventListener("click", function(){
+    console.log("I want to open the box!")
+})
+// 3. Log out "I" want to open the box! when it's clicked
