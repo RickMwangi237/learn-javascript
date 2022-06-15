80fa09e7cc10afc536542921ca34c041948d1bc6
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-15 14:11:58 +0300

    Create loop to sort through fruits array

--- 4. Practice time - part 2/7. Sorting fruits/index.js
+++ 4. Practice time - part 2/7. Sorting fruits/index.js
@@ -3,6 +3,15 @@ let appleShelf = document.getElementById("apple-shelf")
 let orangeShelf = document.getElementById("orange-shelf")
 
 // Create a function that puts the apples onto the appleShelf
+
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
-
+function sortFruits(){
+    for (let i=0; i<fruit.length; i++) {
+        if (fruit[i]==="🍊"){appleShelf.texContent += "🍊"
+        }else if (fruit[i]==="🍎"){orangeShelf.textContent +="🍎"}
+    } ;
+    console.log(appleShelf)
+    console.log(orangeShelf)
+}
+sortFruits()
