ea5b12631edc93e130cc87abab3a433b6074078b
Rick Mwangi <rickmwangi237@protonmail.com>
2022-06-15 14:54:22 +0300

    Create eventlistener on inputnbtn variable

--- 5. Build a Chrome Extension/5. Refactor to addEventListener/index.js
+++ 5. Build a Chrome Extension/5. Refactor to addEventListener/index.js
@@ -1,6 +1,10 @@
 function saveLead() {
-    console.log("Button clicked!")
+    console.log("Button clicked from onclick attribute")
 }
 
+let inputbtn = document.getElementById("input-btn")
 
+inputbtn.addEventListener("click", function(){
+    console.log("Button clicked from event listener!")
+})
 
